06eb7157d16c9fc93aa5d14fe6447a619dfb8dfa
80x86adtcebi <70032712+80x86adtcebi@users.noreply.github.com>
2021-01-13 15:12:55 +0300

    Update ornek15-6.cpp

--- ornek15-6.cpp
+++ ornek15-6.cpp
@@ -1,11 +1,29 @@
 #include <stdio.h>
+
 extern "C" void SelSortASM(int *, int);
+void SelSortC(int *, int);
+
 const int n = 100000;
+
 int main(){
 	int dizi[n], i;
 	for (i = 0; i < n; i++)
 		dizi[i] = i + 1;
 	SelSortASM(dizi, c);
+	//SelSortC(dizi, c);
 	printf("Dizinin ilk elemani: %d, son elemani: %d\n", dizi[0], dizi[n - 1]);
 	return 0;
+}
+
+void SelSortC(int *sdizi, int n) {
+	int i, j, tmp, max;
+	for (i = 0; i < n - 1; i++) {
+		max = i;
+		for (j = i + 1; j < n; j++)
+			if (sdizi[j] > sdizi[max])
+				max = j;
+		tmp = sdizi[max];
+		sdizi[max] = sdizi[i];
+		sdizi[i] = tmp;
+	}
 }
